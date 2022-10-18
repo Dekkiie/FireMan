d2da655a64c38a7d38a327bfe57c0dedced5dfd4
ekkiediller <86023587+ekkiediller@users.noreply.github.com>
2022-10-18 15:30:53 -0300

    aaa

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor.DeviceSimulation;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnWater()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Instantiate(water, touchPos, Quaternion.identity);
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                Destroy(water);
+            }
+        }
+    }
+}
